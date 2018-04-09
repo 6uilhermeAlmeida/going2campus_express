32a3475a1dd49be6d2e59e4f829e19f33c9cc50e
colgatekoala <guicaricas2@gmail.com>
2018-04-09 17:40:02 +0100

    added TripController

---
+++ app/controllers/TripController.js
@@ -0,0 +1,76 @@
+
+var express = require('express');        // call express
+var Trip = require('../models/trip');
+var router = express.Router();              // get an instance of the express Router
+
+
+router.route('/')
+
+    .post(function (req, res) {
+
+        var trip = new Trip(req.body);
+
+        trip.save(function (err) {
+            if (err) {
+                console.log(err.message);
+            }
+
+            res.status(200).json(trip);
+
+        })
+
+    })
+
+    .get(function (req, res) {
+
+        Trip.find(function (err, trips) {
+
+            if (err) {
+                console.log(err);
+            }
+
+            res.json(trips);
+
+        })
+
+    });
+
+router.put('/:id_trip/add_passenger', (req, res) => {
+
+    if (!req.body.passengerId) {
+        res.status(400).send("Bad request, wrong attribute name.");
+        return;
+    }
+
+    Trip.findById(req.params.id_trip, function (err, trip) {
+
+        if (err) {
+            console.log(err);
+            res.status(503).send("Error retrieving data from database.");
+            return;
+        }
+
+        if (!trip) {
+            res.status(404).send("404 Trip not found.");
+            return;
+        }
+
+        if (trip.numberOfSeatsAvailable > 0) {
+            trip.passengers.push(req.body.passengerId);
+            trip.numberOfSeatsAvailable--;
+            trip.save(function (err) {
+                if (err) {
+                    console.log(err);
+                    res.status(503).send("Error saving data to database.");
+                    return;
+                }
+                res.status(200).json(trip);
+            });
+        } else {
+            res.status(202).json({error:"Car has no seats available."});
+        }
+    });
+
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Trip = require('./app/models/trip');
+var TripController = require('./app/controllers/TripController');
 
 mongoose.connect('mongodb://dev:dev@ds223578.mlab.com:23578/going2campus');
 mongoose.connection
@@ -23,86 +24,11 @@ var port = process.env.PORT || 8080;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-router.get('/', function (req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
-});
-
-router.route('/trips')
-
-    .post(function (req, res) {
-
-        var trip = new Trip(req.body);
-
-        trip.save(function (err) {
-            if (err) {
-                console.log(err.message);
-            }
-
-            res.status(200).json(trip);
-
-        })
-
-    })
-
-    .get(function (req, res) {
-
-        Trip.find(function (err, trips) {
-
-            if (err) {
-                console.log(err);
-            }
-
-            res.json(trips);
-
-        })
-
-    });
-
-router.put('/trips/:id_trip/add_passenger', (req, res) => {
-
-    if (!req.body.passengerId) {
-        res.status(400).send("Bad request, wrong attribute name.");
-        return;
-    }
-
-    Trip.findById(req.params.id_trip, function (err, trip) {
-
-        if (err) {
-            console.log(err);
-            res.status(503).send("Error retrieving data from database.");
-            return;
-        }
-
-        if (!trip) {
-            res.status(404).send("404 Trip not found.");
-            return;
-        }
-
-        if (trip.numberOfSeatsAvailable > 0) {
-            trip.passengers.push(req.body.passengerId);
-            trip.numberOfSeatsAvailable--;
-            trip.save(function (err) {
-                if (err) {
-                    console.log(err);
-                    res.status(503).send("Error saving data to database.");
-                    return;
-                }
-                res.status(200).json(trip);
-            });
-        } else {
-            res.status(202).send("Car has no seats available.");
-        }
-    });
-
-});
-
 
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+app.use('/api/trips', TripController);
 
 // START THE SERVER
 // =============================================================================
