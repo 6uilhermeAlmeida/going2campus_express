04cea729482e74b4a16b347518dd7854ec58f22a
José basso <2150649@my.ipleiria.pt>
2018-04-12 02:13:04 +0100

    GET profile of authed user

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -23,5 +23,16 @@ router.get('/', function (req, res) {
     });
 });
 
+router.get('/me', function(req, res, next) {
+
+    User.findById(req.userId, { password: 0 }, function (err, user) {
+      if (err) return res.status(500).send("There was a problem finding the user.");
+      if (!user) return res.status(404).send("No user found.");
+      
+      res.status(200).send(user);
+    });
+  
+});
+
 
 module.exports = router;
