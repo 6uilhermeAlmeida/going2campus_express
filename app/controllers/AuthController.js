e06a70ba97ae4ee81e864ccf1689baabf28749d4
José basso <2150649@my.ipleiria.pt>
2018-04-27 17:01:28 +0100

    OPTIONS ALLOWED

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -33,13 +33,17 @@ router.post('/register', function (req, res) {
                 if (err.errors) {
                     User.schema.eachPath(function (eachPath) {
                         if (err.errors[eachPath]) {
-                            errors.push({ error_message: err.errors[eachPath].message });
+                            errors.push({
+                                error_message: err.errors[eachPath].message
+                            });
                         }
                     });
                 }
 
                 if (err.code == 11000) {
-                    errors.push({ error_message: "There's a user with this e-mail already." });
+                    errors.push({
+                        error_message: "There's a user with this e-mail already."
+                    });
                 }
 
                 res.status(400).json({
@@ -61,7 +65,7 @@ router.post('/register', function (req, res) {
                 from: config.mail_user, // sender address
                 to: user.mail, // list of receivers
                 subject: 'Account confirmation', // Subject line
-                html: '<p>Confirm your account <a href =' + config.host + 'api/auth/verify/' + user.id + '/' + sha256(String(user.id + user.createdAt.getTime() + config.secret)) + '>here</a></p>'// plain text body
+                html: '<p>Confirm your account <a href =' + config.host + 'api/auth/verify/' + user.id + '/' + sha256(String(user.id + user.createdAt.getTime() + config.secret)) + '>here</a></p>' // plain text body
             };
 
             transporter.sendMail(mailOptions, function (err, info) {
@@ -70,41 +74,65 @@ router.post('/register', function (req, res) {
             });
 
             user.password = undefined;
-            res.status(201).json({ message: "We´ve sent a verification e-mail to " + user.mail + ".", user: user });
+            res.status(201).json({
+                message: "We´ve sent a verification e-mail to " + user.mail + ".",
+                user: user
+            });
         });
 });
 
 
 router.post('/login', (req, res) => {
-    User.findOne({ mail: req.body.mail }).select('+password').exec(function (err, user) {
+    User.findOne({
+        mail: req.body.mail
+    }).select('+password').exec(function (err, user) {
 
         if (err) return res.status(500).send('Error on the server.');
         if (!user) return res.status(404).send('No user found.');
-        if (!user.active) return res.status(402).json({ message: "You must verify your e-mail." });
+        if (!user.active) return res.status(402).json({
+            message: "You must verify your e-mail."
+        });
 
         var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-        if (!passwordIsValid) return res.status(401).send({ auth: false, token: null, message: "Invalid password" });
+        if (!passwordIsValid) return res.status(401).send({
+            auth: false,
+            token: null,
+            message: "Invalid password"
+        });
 
-        var token = jwt.sign({ token_user_id: user._id, token_admin: user.admin }, config.secret, {
+        var token = jwt.sign({
+            token_user_id: user._id,
+            token_admin: user.admin
+        }, config.secret, {
             expiresIn: 2628000 // one month in seconds
         });
 
-        res.status(200).send({ auth: true, token: token });
+        res.status(200).send({
+            auth: true,
+            token: token
+        });
     });
 });
 
 router.get('/verify/:id_user/:verification_code', function (req, res) {
 
-
     User.findById(req.params.id_user, function (err, user) {
 
-        if (err) return res.status(500).json({ message: "Error on the serve database." });
-        if (!user) return res.status(404).json({ message: 'User not found.' });
+        if (err) return res.status(500).json({
+            message: "Error on the serve database."
+        });
+        if (!user) return res.status(404).json({
+            message: 'User not found.'
+        });
 
         var expectedToken = sha256(String(user.id + user.createdAt.getTime() + config.secret));
 
-        console.log({ db_status: expectedToken });
-        console.log({ req: req.params.verification_code });
+        console.log({
+            db_status: expectedToken
+        });
+        console.log({
+            req: req.params.verification_code
+        });
 
 
         if (expectedToken === req.params.verification_code) {
@@ -114,16 +142,22 @@ router.get('/verify/:id_user/:verification_code', function (req, res) {
             user.save(function (err, user) {
 
                 if (err) {
-                    return res.status(503).json({ message: "Database error, could not save user." });
+                    return res.status(503).json({
+                        message: "Database error, could not save user."
+                    });
                 }
 
-                return res.status(200).json({ message: "You are now active, enjoy!" });
+                return res.status(200).json({
+                    message: "You are now active, enjoy!"
+                });
 
             });
 
         } else {
 
-            return res.status(401).json({ message: "You need a valid token to activate your account!" });
+            return res.status(401).json({
+                message: "You need a valid token to activate your account!"
+            });
 
         }
 
