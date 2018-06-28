7ab9b251c5cb1ce9cea36592b22f262117988a22
colgatekoala <guicaricas2@gmail.com>
2018-06-28 13:23:30 +0100

    - Securing config

--- app/config/config.js
+++ app/config/config.js
@@ -1,10 +1,10 @@
 module.exports = {
 
-    'secret': 'roubaram_me_a_bicicreta',
-    'database': 'mongodb://dev:dev@ds223578.mlab.com:23578/going2campus',
-    'mail_user' : 'going2campus@gmail.com',
-    'mail_password' : 'going2campus_bicla!',
-    'host' : 'http://localhost:8080/'
+    'secret': 'x',
+    'database': 'mongodb://x:x/x',
+    'mail_user' : 'xx@gmail.com',
+    'mail_password' : 'xx',
+    'host' : 'http://x:8080/'
 
 
 };
